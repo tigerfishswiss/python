0ab600299686874bbb4ed44866e9fad6a9f65e56
Tigerfish <tigerfish.download@gmail.com>
2022-06-10 23:09:18 +0200

    V18

---
+++ pipelinepoc/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="pipelinepoc_tigerfish",
+    version="0.0.1",
+    author="Tigerfish",
+    author_email="tigerfish.download@gmail.com",
+    description="A small example package",
+    long_description="long_description",
+    long_description_content_type="text/markdown",
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
